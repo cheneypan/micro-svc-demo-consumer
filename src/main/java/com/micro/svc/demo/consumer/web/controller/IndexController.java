688f1fcdf437e3c746078303f442717851108727
langyilin <416739124@qq.com>
2017-08-30 11:29:07 +0800

    Update IndexController.java

--- src/main/java/com/micro/svc/demo/consumer/web/controller/IndexController.java
+++ src/main/java/com/micro/svc/demo/consumer/web/controller/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by cheney on 2017/8/27.
+ * Created by cheney on 2017/8/271.
  */
 @RestController
 public class IndexController {
